e4e9b2a409ab6f1274db9037bd0d54fb6ac09ec8
Bohnna Chhim <info@bohnna.com>
2014-07-24 15:03:04 -0400

    added ex2 directory

---
+++ ex2/ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+	puts("Hello World.");
+	puts("HELLO BRO.");
+	puts("BRO, HELLO.");
+	puts("LEARNING C THE HARD WAY");
+	puts("GOOD LUCK TO YOU");
+	puts("DONT GIVE UP.");
+	return 0;
+}
